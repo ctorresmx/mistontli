769e88e9a5822ca185e981c5506e0577f8139d5a
Carlos Torres <ctorres@nearsoft.com>
2017-04-10 00:26:35 -0500

    Refactored all the code.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,12 @@
 # TODO
 
-## Saturday 04/08/17 at 12:52AM (4)
+## Saturday 04/08/17 at 12:52AM
 - [X] mistontli/mistontli.py:37   Refactor this method, it could probably be less convoluted.
 - [X] mistontli/mistontli.py:47   This could be improved with a more robust way to determine if the
 - [X] mistontli/mistontli.py:62   Refactor all of this into auxiliary methods. Auxiliary methods
 - [X] mistontli/mistontli.py:83   Think of a way to remove the need of calling the _show_board out
+
+## Monday 04/10/17 at 12:22AM
+
+- [ ] mistontli/mistontli.py:8     Document everything
+- [ ] mistontli/mistontli.py:109   Investigate if this can be a class variable and point to
--- mistontli/mistontli.py
+++ mistontli/mistontli.py
@@ -3,45 +3,100 @@ import fire
 import mistontli
 import random
 import copy
+import time
 
-# TODO: Make big refactor for all this mess
+# TODO: Document everything
 
 
 class Player:
     symbols = ['X', 'O']
 
-    @classmethod
-    def players_creator(cls):
-        """Creates two players asking their name. Randomly assigns a playable symbol."""
-        first_player_name = input('Enter first player name: ')
-        second_player_name = input('Enter second player name: ')
-
-        coin_toss = round(random.uniform(0, 1))
-        first_player_symbol = cls.symbols[coin_toss]
-        second_player_symbol = cls.symbols[abs(1 - coin_toss)]
-
-        first_player = cls(first_player_name, first_player_symbol)
-        second_player = cls(second_player_name, second_player_symbol)
-
-        return first_player, second_player
-
-    @classmethod
-    def players_ai_creator(cls):
-        """Creates two players asking their name. Randomly assigns a playable symbol."""
-        player_name = input('Enter your name: ')
-
-        coin_toss = round(random.uniform(0, 1))
-        player_symbol = cls.symbols[coin_toss]
-        computer_symbol = cls.symbols[abs(1 - coin_toss)]
-
-        player = cls(player_name, player_symbol)
-        computer = cls('Mistontli', computer_symbol)
-
-        return player, computer
-
     def __init__(self, name, symbol):
         self.name = name
         self.symbol = symbol
+
+    def opponents_symbol(self):
+        if self.symbol == 'X':
+            return 'O'
+        return 'X'
+
+    def get_move(self, board):
+        raise NotImplementedError
+
+
+class ClassicAIPlayer(Player):
+    name = 'Mistontli Classical'
+
+    def __init__(self, name, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+        """ This gets the next AI move inside the board.
+
+        According to the algorithm by Al Sweigart from the 2017 book "Invent
+        your own computer games with python, 4th edition."
+        """
+
+        print('Thinking...')
+        time.sleep(1)
+
+        # Tries to win in the next turn
+        for i in range(9):
+            if board[i] == ' ':
+                board_copy = copy.deepcopy(board)
+                board_copy[i] = self.symbol
+
+                if Mistontli.determine_winning(self.symbol, board_copy):
+                    return i
+
+        # Tries to block the player
+        for i in range(9):
+            if board[i] == ' ':
+                board_copy = copy.deepcopy(board)
+                board_copy[i] = self.opponents_symbol()
+
+                if Mistontli.determine_winning(self.opponents_symbol(), board_copy):
+                    return i
+
+        # Tries the corners randomly
+        corners = [0, 2, 6, 8]
+        random.shuffle(corners)
+        for corner in corners:
+            if board[corner] == ' ':
+                return corner
+
+        # Tries the center
+        if board[4] == ' ':
+            return 4
+
+        # Tries the sides randomly
+        sides = [1, 3, 5, 7]
+        random.shuffle(sides)
+        for side in sides:
+            if board[side] == ' ':
+                return side
+
+
+class HumanPlayer(Player):
+
+    def get_move(self, board):
+        move = input('{} pick your move (1 - 9): '.format(self.name))
+
+        try:
+            move = int(move)
+
+            if board[move - 1] == ' ':
+                return move - 1
+            else:
+                print('That cell is already filled!')
+                return self.get_move(board)
+
+        except ValueError:
+            print('You should enter a number between 1 and 9')
+            return self.get_move(board)
+        except IndexError:
+            print('You should enter a number between 1 and 9')
+            return self.get_move(board)
 
 
 class Mistontli:
@@ -55,53 +110,14 @@ class Mistontli:
         self.winner = None
         self.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-    def _get_next_move(self):
-        """ This gets the next AI move inside the board.
+        # TODO: Investigate if this can be a class variable and point to
+        # these functions.
+        self.game_modes = {'vs': self._prepare_2_player_game,
+                           'classicAI': self._prepare_classical_ai_game,
+                           }
 
-        According to the algorithm by Al Sweigart from the 2017 book "Invent
-        your own computer games with python, 4th edition."
-        """
-
-        # Tries to win in the next turn
-        for i in range(9):
-            if self.board[i] == ' ':
-                board = copy.deepcopy(self.board)
-                board[i] = self.second_player.symbol
-
-                if self._determine_winning(self.second_player.symbol, board):
-                    return i
-
-        # Tries to block the player
-        for i in range(9):
-            if self.board[i] == ' ':
-                board = copy.deepcopy(self.board)
-                board[i] = self.first_player.symbol
-
-                if self._determine_winning(self.first_player.symbol, board):
-                    return i
-
-        # Tries the corners randomly
-        corners = [0, 2, 6, 8]
-        random.shuffle(corners)
-        for corner in corners:
-            if self.board[corner] == ' ':
-                return corner
-
-        # Tries the center
-        if self.board[4] == ' ':
-            return 4
-
-        # Tries the sides randomly
-        sides = [1, 3, 5, 7]
-        random.suffle(sides)
-        for side in sides:
-            if self.board[side] == ' ':
-                return side
-
-    def _determine_winning(self, letter, board=None):
-        if not board:
-            board = self.board
-
+    @staticmethod
+    def determine_winning(letter, board):
         return ((board[0] == letter and board[1] == letter and
                  board[2] == letter) or  # Top
                 (board[3] == letter and board[4] == letter and
@@ -128,70 +144,61 @@ class Mistontli:
                 print('|\n - - -')
         print('')
 
-    def _get_move(self, player):
-        """Given a player, asks for a possible board move."""
+    def _prepare_2_player_game(self):
+        """Creates two players asking their name. Randomly assigns a playable symbol."""
+        first_player_name = input('Enter first player name: ')
+        second_player_name = input('Enter second player name: ')
 
-        move = input('{} pick your move (1 - 9): '.format(player.name))
+        coin_toss = round(random.uniform(0, 1))
+        first_player_symbol = Player.symbols[coin_toss]
+        second_player_symbol = Player.symbols[abs(1 - coin_toss)]
 
-        try:
-            move = int(move)
-        except ValueError:
-            print('You should enter a number between 1 and 9')
-            return self._get_move(player)
+        self.first_player = HumanPlayer(first_player_name, first_player_symbol)
+        self.second_player = HumanPlayer(second_player_name, second_player_symbol)
 
-        if self.board[move - 1] == ' ':
-            self.board[move - 1] = player.symbol
-        else:
-            print('That cell is already filled!')
-            return self._get_move(player)
-
-    def _prepare_game(self):
-        self.first_player, self.second_player = Player.players_creator()
         self.current_player = self.first_player if self.first_player.symbol == 'X' else self.second_player
 
-    def _prepare_ai_game(self):
-        self.first_player, self.second_player = Player.players_ai_creator()
-        self.current_player = self.first_player
-
-    def _game_step(self):
-        self._show_board()
-        self._get_move(self.current_player)
-
-        if self._determine_winning(self.current_player.symbol):
-            self.active_game = False
-            self.winner = True
-            return
-
-        if ' ' not in self.board:
-            self.active_game = False
-
-        self.current_player = self.second_player if self.current_player is self.first_player else self.first_player
-
-    def _ai_game_step(self):
-        self._show_board()
-
-        if self.current_player is self.first_player:
-            self._get_move(self.current_player)
-        else:
-            print(self._get_next_move())
-            self.board[self._get_next_move()] = self.second_player.symbol
-
-        if self._determine_winning(self.current_player.symbol):
-            self.active_game = False
-            self.winner = True
-            return
-
-        if ' ' not in self.board:
-            self.active_game = False
-
-        self.current_player = self.second_player if self.current_player is self.first_player else self.first_player
-
-    def start_game(self):
-        print('Welcome to Mistontli!')
-        self._prepare_game()
         print('{} you play with {}'.format(self.first_player.name, self.first_player.symbol))
         print('{} you play with {}'.format(self.second_player.name, self.second_player.symbol))
         print('{} you go first'.format(self.current_player.name))
+
+    def _prepare_classical_ai_game(self):
+        """Creates a human player and a classicAI player. Randomly assigns a playable symbol."""
+        player_name = input('Enter your name: ')
+
+        coin_toss = round(random.uniform(0, 1))
+        player_symbol = Player.symbols[coin_toss]
+        computer_symbol = Player.symbols[abs(1 - coin_toss)]
+
+        self.first_player = HumanPlayer(player_name, player_symbol)
+        self.second_player = ClassicAIPlayer('Mistontli', computer_symbol)
+
+        self.current_player = self.first_player
+
+        print('{} you play with {}'.format(self.first_player.name, self.first_player.symbol))
+        print('I will play with {}'.format(self.second_player.name, self.second_player.symbol))
+        print('You go first'.format(self.first_player.name))
+
+    def _game_step(self):
+        self._show_board()
+        move = self.current_player.get_move(self.board)
+
+        self.board[move] = self.current_player.symbol
+
+        if Mistontli.determine_winning(self.current_player.symbol, self.board):
+            self.active_game = False
+            self.winner = True
+            return
+
+        if ' ' not in self.board:
+            self.active_game = False
+
+        self.current_player = self.second_player if self.current_player is self.first_player else self.first_player
+
+    def start_game(self, mode='vs'):
+        print('Welcome to Mistontli!')
+        print('You are going to play in {} mode'.format(mode))
+        self.game_modes[mode]()
 
         while self.active_game:
             self._game_step()
@@ -199,27 +206,6 @@ class Mistontli:
         self._show_board()
         if self.winner:
             print('{} you win!'.format(self.current_player.name))
-        else:
-            print('Draw!')
-
-        print('Game has finished. Thanks for playing!')
-
-    def start_ai_game(self):
-        print('Welcome to Mistontli!')
-        self._prepare_ai_game()
-        print('{} you play with {}'.format(self.first_player.name, self.first_player.symbol))
-        print('I will play with {}'.format(self.second_player.name, self.second_player.symbol))
-        print('You go first'.format(self.first_player.name))
-
-        while self.active_game:
-            self._ai_game_step()
-
-        self._show_board()
-        if self.winner:
-            if self.current_player is self.first_player:
-                print('You win!')
-            else:
-                print('You lose!')
         else:
             print('Draw!')
 
